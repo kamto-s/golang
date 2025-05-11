1500e2fd91eaa73dbf11e1d9a37a28c8921b9bb8
kamto-s <kamto.076s@gmail.com>
2025-05-12 00:34:23 +0700

    modified:   main.go

--- main.go
+++ main.go
@@ -3,48 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	angka := 4
-
-	switch {
-	case angka > 1:
-		{
-			fmt.Println("Angka lebih besar daripada 1")
-			break
-		}
-	case angka > 2:
-		{
-			fmt.Println("Angka lebih besar daripada 2")
-			break
-		}
-	default:
-		{
-			fmt.Println("Angka tidak dikenali")
-		}
+	fmt.Println("==========================")
+	i := 8
+	for i <= 10 {
+		fmt.Printf("Nilai ke-%d \n", i)
+		i++
 	}
 
-	nilai := 40
+	fmt.Println("==========================")
 
-	switch {
-	case nilai >= 90 && nilai <= 100:
-		{
-			fmt.Println("Nilai A")
-			break
-		}
-	case nilai >= 80 && nilai < 90:
-		{
-			fmt.Println("Nilai B")
-			break
-		}
-	case nilai >= 70 && nilai < 80:
-		{
-			fmt.Println("Nilai C")
-			break
-		}
-	default:
-		{
-			fmt.Println("Nilai Anda tidak memenuhi syarat")
-			break
-		}
-
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("Nilai ke-%v \n", i)
 	}
+
+	fmt.Println("==========================")
+
+	for i := 1; i <= 10; i += 2 {
+		fmt.Printf("Nilai ke-%v \n", i)
+	}
+
+	fmt.Println("==========================")
 }

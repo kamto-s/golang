73ce5733336b98517fae27d6173d5f0c251ddc6b
kamto-s <kamto.076s@gmail.com>
2025-05-11 18:20:58 +0700

    modified:   main.go

--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	// operator logika
-	/*
-		- AND (&&)
-		- OR (||)
-		- NOT (!)
-	*/
+	//percabangan if else
+	nilai := 65
 
-	fmt.Printf("true && false: %t \n", 1 == 1 && 3 == 2)
-	fmt.Printf("true || false: %t \n", 1 == 1 || 3 == 2)
-	fmt.Printf("!true: %t \n", !(1 == 3))
+	if nilai >= 90 && nilai <= 100 {
+		fmt.Println("Nilai Anda A")
+	} else if nilai >= 80 && nilai < 90 {
+		fmt.Println("Nilai Anda AB")
+	} else if nilai >= 70 && nilai < 80 {
+		fmt.Println("Nilai Anda B")
+	} else if nilai >= 60 && nilai < 70 {
+		fmt.Println("Nilai Anda C")
+	} else {
+		fmt.Println("Anda Tidak Lulus")
+	}
 }
